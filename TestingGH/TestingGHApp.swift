5195f208e02c0348fcb0ab792892da9c356acc73
Julia Spehlmann <julialeigh.spehlmann@gmail.com>
2022-12-06 16:05:20 -0500

    Initial Commit

---
+++ TestingGH/TestingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGHApp.swift
+//  TestingGH
+//
+//  Created by Julia Spehlmann on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
